4a2d7de7793159d8095c2811d1d962c80cb7338c
Thimo <thibmorozier@live.nl>
2020-10-13 00:23:32 +0200

    Lib rewrite - Comment out old source for ref - Write PurePascal MurMurHash1 implementation - Add project files for easy dev - Add placeholder methods for TODO work

--- MurmurHash.pas
+++ MurmurHash.pas
@@ -1,13 +1,62 @@
+{------------------------------------------------------------------------------
+  MurmurHash was written by Austin Appleby, and is placed in the public
+  domain. The author hereby disclaims copyright to this source code.
+
+  Note - This code makes a few assumptions about how your machine behaves -
+
+  1. We can read a 4-byte value from any address without crashing
+  2. sizeof(int) == 4
+
+  And it has a few limitations -
+
+  1. It will not work incrementally.
+  2. It will not produce the same results on little-endian and big-endian
+     machines.
+
+  Note :
+    The x86 and x64 versions do _not_ produce the same results, as the
+    algorithms are optimized for their respective platforms. You can still
+    compile and run any of them on any platform, but your performance with the
+    non-native version will be less than optimal.
+}
+
 unit MurmurHash;
-
 interface
-
-{$IFNDEF Unicode}
 type
-	UnicodeString = WideString;
-	UInt64 = Int64;
-{$ENDIF}
+  UInt32Array  = array[0..8191] of UInt32;
+  PUInt32Array = ^UInt32Array;
+  UInt64Array  = array[0..4095] of UInt64;
+  PUInt64Array = ^UInt64Array;
 
+  TMurMur1 = class
+  strict private
+    const m: Cardinal = $c6a4a793;
+    const r: Integer  = 16;
+  public
+    class function Hash(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+    class function HashAligned(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+  end;
+
+  TMurMur2 = class
+  strict private
+  public
+    class function Hash(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+    class function Hash64A(const Key; KeyLen: Integer; const Seed: UInt64): UInt64;
+    class function Hash64B(const Key; KeyLen: Integer; const Seed: UInt64): UInt64;
+    class function HashA(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+    class function HashNeutral(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+    class function HashAligned(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+  end;
+
+  TMurMur3 = class
+  strict private
+  public
+    class procedure Hash_x86_32(const Key; KeyLen: Integer; const Seed: UInt32; OutVal: Pointer);
+    class procedure Hash_x86_128(const Key; KeyLen: Integer; const Seed: UInt32; OutVal: Pointer);
+    class procedure Hash_x64_128(const Key; KeyLen: Integer; const Seed: UInt32; OutVal: Pointer);
+  end;
+
+(*
 type
 	TMurmur3 = class(TObject)
 	protected
@@ -20,9 +69,277 @@ type
 		class function HashData32(const Key; KeyLen: LongWord; const Seed: LongWord): LongWord;
 		class function HashString32(const Key: UnicodeString; const Seed: LongWord): LongWord;
 	end;
-
+*)
 implementation
+uses
+  SysUtils, Math;
 
+{ Utils }
+
+function ROTL32(x: UInt32; n: Int8): UInt32; inline;
+begin
+  Result := (x shl n) or (x shr (32 - n));
+end;
+
+function ROTL64(x: UInt64; n: Int8): UInt64; inline;
+begin
+  Result := (x shl n) or (x shr (64 - n));
+end;
+
+{------------------------------------------------------------------------------
+  Finalization mix - force all bits of a hash block to avalanche
+}
+
+function fmix32(h: UInt32): UInt32; inline;
+begin
+  h := h xor (h shr 16);
+  h := h * $85ebca6b;
+  h := h xor (h shr 13);
+  h := h * $c2b2ae35;
+  h := h xor (h shr 16);
+
+  Result := h;
+end;
+
+function fmix64(h: UInt64): UInt64; inline;
+begin
+  h := h xor (h shr 33);
+  h := h * $ff51afd7ed558ccd;
+  h := h xor (h shr 33);
+  h := h * $c4ceb9fe1a85ec53;
+  h := h xor (h shr 33);
+
+  Result := h;
+end;
+
+{------------------------------------------------------------------------------
+  Block read - if your platform needs to do endian-swapping or can only
+  handle aligned reads, do the conversion here
+}
+
+function getblock32(const ptr: PUInt32Array; idx: Integer): UInt32; inline;
+begin
+  Result := ptr[idx];
+end;
+
+function getblock64(const ptr: PUInt64Array; idx: Integer): UInt64; inline;
+begin
+  Result := ptr[idx];
+end;
+
+{ TMurMur1 }
+// https://github.com/rurban/smhasher/blob/master/MurmurHash1.cpp
+// objsize: 0-0x157: 343
+class function TMurMur1.Hash(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+var
+  hash,
+  k:    Cardinal;
+  data: PByteArray;
+  i,
+  len:  Integer;
+begin
+  hash := Seed xor (KeyLen * m);
+  data := PByteArray(@Key);
+  len  := KeyLen;
+  i    := 0;
+
+  while len >= 4 do
+  begin
+    k := PCardinal(@(data[i]))^;
+    hash := hash + k;
+    hash := hash * m;
+    hash := hash xor (hash shr r);
+
+    Inc(i, 4);
+    Dec(len, 4);
+  end;
+
+  case len of
+    3:
+      hash := hash + (data[i + 2] shl r);
+    2:
+      hash := hash + (data[i + 1] shl 8);
+    1:
+    begin
+      hash := hash + data[i];
+      hash := hash * m;
+      hash := hash xor (hash shr r);
+    end;
+  end;
+
+  hash := hash * m;
+  hash := hash xor (hash shr 10);
+  hash := hash * m;
+  hash := hash xor (hash shr 17);
+
+  Result := hash;
+end;
+
+{------------------------------------------------------------------------------
+  TMurmur1.HashAligned, by Austin Appleby
+
+  Same algorithm as TMurmur1.Hash, but only does aligned reads - should be
+  safer on certain platforms.
+
+  Performance should be equal to or better than the simple version.
+  objsize: 0x160-0x4e3: 899
+}
+class function TMurMur1.HashAligned(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+var
+  hash,
+  t, d:    Cardinal;
+  data:    PByteArray;
+  i,
+  len,
+  align,
+  sl, sr,
+  pack:    Integer;
+begin
+  hash  := Seed xor (KeyLen * m);
+  data  := PByteArray(@Key);
+  len   := KeyLen;
+  i     := 0;
+  align := UInt64(data) and 3;
+
+  if (align > 0) and (len >= 4) then
+  begin
+    // Pre-load the temp registers
+    t := 0;
+    d := 0;
+
+    case align of
+      1: t := t or (data[i + 2] shl r);
+      2: t := t or (data[i + 1] shl 8);
+      3: t := t or data[i + 0];
+    end;
+
+    t := t shl (8 * align);
+    Inc(i, 4 - align);
+    Dec(len, 4 - align);
+
+    sl := 8 * (4 - align);
+    sr := 8 * align;
+
+    // Mix
+    while len >= 4 do
+    begin
+      d    := PCardinal(@(data[i]))^;
+      t    := (t shr sr) or (d shl sl);
+      hash := hash + t;
+      hash := hash * m;
+      hash := hash xor (hash shr r);
+      t    := d;
+
+      Inc(i, 4);
+      Dec(len, 4);
+    end;
+
+    pack := Min(len, align);
+
+    d := 0;
+
+    case pack of
+      3:
+        d := d or (data[i + 2] shl r);
+      2:
+        d := d or (data[i + 1] shl 8);
+      1:
+        d := d or data[i];
+      0:
+      begin
+        hash := hash + ((r shr sr) or (d shl sl));
+        hash := hash * m;
+        hash := hash xor (hash shr r);
+      end;
+    end;
+
+    Inc(i, pack);
+    Dec(len, pack);
+  end else
+  begin
+    while len >= 4 do
+    begin
+      hash := hash + PCardinal(@(data[i]))^;
+      hash := hash * m;
+      hash := hash xor (hash shr r);
+
+      Inc(i, 4);
+      Dec(len, 4);
+    end;
+  end;
+
+  // Handle tail bytes
+  case len of
+    3:
+      hash := hash + (data[i + 2] shl r);
+    2:
+      hash := hash + (data[i + 1] shl 8);
+    1:
+    begin
+      hash := hash + data[i];
+      hash := hash * m;
+      hash := hash xor (hash shr r);
+    end;
+  end;
+
+  hash := hash * m;
+  hash := hash xor (hash shr 10);
+  hash := hash * m;
+  hash := hash xor (hash shr 17);
+
+  Result := hash;
+end;
+
+{ TMurMur2 }
+// https://github.com/rurban/smhasher/blob/master/MurmurHash2.cpp
+class function TMurMur2.Hash(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+begin
+
+end;
+
+class function TMurMur2.Hash64A(const Key; KeyLen: Integer; const Seed: UInt64): UInt64;
+begin
+
+end;
+
+class function TMurMur2.Hash64B(const Key; KeyLen: Integer; const Seed: UInt64): UInt64;
+begin
+
+end;
+
+class function TMurMur2.HashA(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+begin
+
+end;
+
+class function TMurMur2.HashNeutral(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+begin
+
+end;
+
+class function TMurMur2.HashAligned(const Key; KeyLen: Integer; const Seed: UInt32): UInt32;
+begin
+
+end;
+
+{ TMurMur3 }
+// https://github.com/rurban/smhasher/blob/master/MurmurHash3.cpp
+class procedure TMurMur3.Hash_x86_32(const Key; KeyLen: Integer; const Seed: UInt32; OutVal: Pointer);
+begin
+
+end;
+
+class procedure TMurMur3.Hash_x86_128(const Key; KeyLen: Integer; const Seed: UInt32; OutVal: Pointer);
+begin
+
+end;
+
+class procedure TMurMur3.Hash_x64_128(const Key; KeyLen: Integer; const Seed: UInt32; OutVal: Pointer);
+begin
+
+end;
+
+(*
 uses
 	SysUtils, Windows
 {$IFDEF UnitTests}, MurmurHashTests{$ENDIF};
@@ -176,5 +493,6 @@ class function TMurmur3.HashData128_x86(const Key; KeyLen: LongWord; const Seed:
 begin
 	raise Exception.Create('Not implemented');
 end;
+*)
 
 end.
