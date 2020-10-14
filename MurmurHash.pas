7bcf3affa2867ba116db8db33dcd5bd07ea2c604
Thimo <thibmorozier@live.nl>
2020-10-14 18:37:20 +0200

    Add sample implementation of MurmurHash2 in TMurMur2

--- MurmurHash.pas
+++ MurmurHash.pas
@@ -22,6 +22,9 @@
 
 unit MurMurHash;
 interface
+uses
+  SysUtils;
+
 type
   UInt32Array  = array[0..8191] of UInt32;
   PUInt32Array = ^UInt32Array;
@@ -34,7 +37,32 @@ type
     class function HashAligned(const Key; KeyLen: UInt32; const Seed: UInt32): UInt32;
   end;
 
-  TMurMur2 = class
+  {
+    Includes a sample implementation of MurmurHash2A designed to work incrementally.
+
+    Usage:
+      var
+        hasher: TMurMur2;
+        hash:   UInt32;
+      begin
+        hasher := hasher.Create(seed);
+        hasher.Add(data1, size1);
+        hasher.Add(data2, size2);
+        // ...
+        hasher.Add(dataN, sizeN);
+        hash := hasher.Hash;
+      end;
+  }
+  TMurMur2 = class(TObject)
+  strict private
+    fHash,
+    fTail,
+    fCount,
+    fSize:  UInt32;
+    const
+      m: UInt32 = $5bd1e995;
+      r: UInt32 = 24;
+    procedure MixTail(var data: PByteArray; var dataLen, idx: Integer);
   public
     class function Hash(const Key; KeyLen: UInt32; const Seed: UInt32): UInt32;
     class function HashA(const Key; KeyLen: UInt32; const Seed: UInt32): UInt32;
@@ -42,6 +70,10 @@ type
     class function Hash64B(const Key; KeyLen: UInt32; const Seed: UInt64): UInt64;
     class function HashNeutral(const Key; KeyLen: UInt32; const Seed: UInt32): UInt32;
     class function HashAligned(const Key; KeyLen: UInt32; const Seed: UInt32): UInt32;
+
+    constructor Create(Seed: UInt32);
+    procedure Add(const value; dataLen: UInt32);
+    function Calculate: UInt32;
   end;
 
   TMurMur3 = class
@@ -54,7 +86,7 @@ type
 
 implementation
 uses
-  SysUtils, Math;
+  Math;
 
 { Utils }
 const
@@ -345,6 +377,8 @@ end;
   This variant fixes a minor issue where null keys were more likely to
   collide with each other than expected, and also makes the function
   more amenable to incremental implementations.
+
+  objsize: 0x500-0x697: 407
 }
 class function TMurMur2.HashA(const Key; KeyLen: UInt32; const Seed: UInt32): UInt32;
 const
@@ -733,21 +767,313 @@ begin
   end;
 end;
 
+constructor TMurMur2.Create(Seed: UInt32);
+begin
+  fHash  = Seed;
+  fTail  = 0;
+  fCount = 0;
+  fSize  = 0;
+end;
+
+procedure TMurMur2.Add(const value; dataLen: UInt32);
+var
+  data:   PByteArray;
+  len, k: UInt32;
+  i:      Integer;
+begin
+  fSize := fSize + dataLen;
+  len   := dataLen;
+  data  := PByteArray(@value);
+
+  MixTail(data, len);
+
+  while len >= 4 do
+  begin
+    k = PUInt32(@(data[i]))^;
+
+    mmix(fHash, k, m, r);
+
+    Inc(i, 4);
+    Dec(dataLen, 4);
+  end;
+
+  MixTail(data, len);
+end;
+
+function TMurMur2.Calculate: UInt32;
+begin
+  mmix(fHash, fTail, m, r);
+  mmix(fHash, fSize, m, r);
+
+  fHash := fHash xor (fHash shr 13);
+  fHash := fHash * m;
+  fHash := fHash xor (fHash shr 15);
+
+  Result := fHash;
+end;
+
+procedure TMurMur2.MixTail(const data: PByteArray; var dataLen, idx: Integer);
+begin
+  while (dataLen > 0) and ((dataLen < 4) or (fCount > 0)) do
+  begin
+    fTail := fTail or (PByte(@(data[idx]))^ shl (fCount * SH_AMNT_8));
+    Inc(idx);
+    Inc(fCount);
+    Dec(dataLen);
+
+    if fCount = 4 then
+    begin
+        mmix(fHash, fTail, m, r);
+        fTail  := 0;
+        fCount := 0;
+    end;
+  end;
+end;
+
 { TMurMur3 }
 // https://github.com/rurban/smhasher/blob/master/MurmurHash3.cpp
+//-----------------------------------------------------------------------------
+// objsize: 0x0-0x15f: 351
 class procedure TMurMur3.Hash_x86_32(const Key; KeyLen: UInt32; const Seed: UInt32; OutVal: Pointer);
 begin
+(*
+  const uint8_t * data = (const uint8_t* )key;
+  const int nblocks = len / 4;
 
+  uint32_t h1 = seed;
+
+  const uint32_t c1 = 0xcc9e2d51;
+  const uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t * )(data + nblocks*4);
+
+  for(int i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock32(blocks,i);
+
+    k1 *= c1;
+    k1 = ROTL32(k1,15);
+    k1 *= c2;
+
+    h1 ^= k1;
+    h1 = ROTL32(h1,13);
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t* )(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+  case 2: k1 ^= tail[1] << 8;
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix32(h1);
+
+  *(uint32_t* )out = h1;
+*)
 end;
 
+//-----------------------------------------------------------------------------
+// objsize: 0x160-0x4bb: 859
 class procedure TMurMur3.Hash_x86_128(const Key; KeyLen: UInt32; const Seed: UInt32; OutVal: Pointer);
 begin
+(*
+  const uint8_t * data = (const uint8_t* )key;
+  const int nblocks = len / 16;
 
+  uint32_t h1 = seed;
+  uint32_t h2 = seed;
+  uint32_t h3 = seed;
+  uint32_t h4 = seed;
+
+  const uint32_t c1 = 0x239b961b;
+  const uint32_t c2 = 0xab0e9789;
+  const uint32_t c3 = 0x38b34ae5;
+  const uint32_t c4 = 0xa1e38b93;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t * )(data + nblocks*16);
+
+  for(int i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock32(blocks,i*4+0);
+    uint32_t k2 = getblock32(blocks,i*4+1);
+    uint32_t k3 = getblock32(blocks,i*4+2);
+    uint32_t k4 = getblock32(blocks,i*4+3);
+
+    k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+
+    h1 = ROTL32(h1,19); h1 += h2; h1 = h1*5+0x561ccd1b;
+
+    k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
+
+    h2 = ROTL32(h2,17); h2 += h3; h2 = h2*5+0x0bcaa747;
+
+    k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
+
+    h3 = ROTL32(h3,15); h3 += h4; h3 = h3*5+0x96cd1c35;
+
+    k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
+
+    h4 = ROTL32(h4,13); h4 += h1; h4 = h4*5+0x32ac3b17;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t* )(data + nblocks*16);
+
+  uint32_t k1 = 0;
+  uint32_t k2 = 0;
+  uint32_t k3 = 0;
+  uint32_t k4 = 0;
+
+  switch(len & 15)
+  {
+  case 15: k4 ^= tail[14] << 16;
+  case 14: k4 ^= tail[13] << 8;
+  case 13: k4 ^= tail[12] << 0;
+           k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
+
+  case 12: k3 ^= tail[11] << 24;
+  case 11: k3 ^= tail[10] << 16;
+  case 10: k3 ^= tail[ 9] << 8;
+  case  9: k3 ^= tail[ 8] << 0;
+           k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
+
+  case  8: k2 ^= tail[ 7] << 24;
+  case  7: k2 ^= tail[ 6] << 16;
+  case  6: k2 ^= tail[ 5] << 8;
+  case  5: k2 ^= tail[ 4] << 0;
+           k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
+
+  case  4: k1 ^= tail[ 3] << 24;
+  case  3: k1 ^= tail[ 2] << 16;
+  case  2: k1 ^= tail[ 1] << 8;
+  case  1: k1 ^= tail[ 0] << 0;
+           k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len; h2 ^= len; h3 ^= len; h4 ^= len;
+
+  h1 += h2; h1 += h3; h1 += h4;
+  h2 += h1; h3 += h1; h4 += h1;
+
+  h1 = fmix32(h1);
+  h2 = fmix32(h2);
+  h3 = fmix32(h3);
+  h4 = fmix32(h4);
+
+  h1 += h2; h1 += h3; h1 += h4;
+  h2 += h1; h3 += h1; h4 += h1;
+
+  ((uint32_t* )out)[0] = h1;
+  ((uint32_t* )out)[1] = h2;
+  ((uint32_t* )out)[2] = h3;
+  ((uint32_t* )out)[3] = h4;
+*)
 end;
 
+//-----------------------------------------------------------------------------
+// objsize: 0x500-0x7bb: 699
 class procedure TMurMur3.Hash_x64_128(const Key; KeyLen: UInt32; const Seed: UInt32; OutVal: Pointer);
 begin
+(*
+  const uint8_t * data = (const uint8_t* )key;
+  const int nblocks = len / 16;
 
+  uint64_t h1 = seed;
+  uint64_t h2 = seed;
+
+  const uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
+  const uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
+
+  //----------
+  // body
+
+  const uint64_t * blocks = (const uint64_t * )(data);
+
+  for(int i = 0; i < nblocks; i++)
+  {
+    uint64_t k1 = getblock64(blocks,i*2+0);
+    uint64_t k2 = getblock64(blocks,i*2+1);
+
+    k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+
+    h1 = ROTL64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+
+    k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+
+    h2 = ROTL64(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t* )(data + nblocks*16);
+
+  uint64_t k1 = 0;
+  uint64_t k2 = 0;
+
+  switch(len & 15)
+  {
+  case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+  case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+  case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+  case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+  case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+  case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
+  case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
+           k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+
+  case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
+  case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
+  case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
+  case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
+  case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
+  case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
+  case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
+  case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
+           k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+  h1 ^= len; h2 ^= len;
+
+  h1 += h2;
+  h2 += h1;
+
+  h1 = fmix64(h1);
+  h2 = fmix64(h2);
+
+  h1 += h2;
+  h2 += h1;
+
+  ((uint64_t* )out)[0] = h1;
+  ((uint64_t* )out)[1] = h2;
+*)
 end;
 
 class function TMurMur3.Hash(const Key; KeyLen: UInt32; const Seed: UInt32): UInt64;
